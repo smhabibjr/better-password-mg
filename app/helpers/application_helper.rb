6e13c6c850da6a924d960dbe6044132ae0473ff7
smhabibjr <smhabib.abir2030@gmail.com>
2026-03-23 14:47:44 +0600

    finished authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
           "<i class=\"bi bi-safe-fill me-2\"></i> BatterPass".html_safe
         end
       end
+
+      def account_page?
+        current_page?(edit_user_registration_path)
+      end
 end
